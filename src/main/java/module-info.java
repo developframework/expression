3982ad2097764319a55ad85f3dbee8af4ffcd136
qiushui <qiushui@zgpgc.com>
2020-03-11 14:53:02 +0800

    1.5.0 支持模块化

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author qiushui on 2020-03-11.
+ */
+module expression {
+    requires lombok;
+    requires org.apache.commons.lang3;
+
+    exports com.github.developframework.expression.exception;
+    exports com.github.developframework.expression;
+}
